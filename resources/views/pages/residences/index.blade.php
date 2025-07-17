e0eab92c58c13efecff277d1c16ad9d54e2500ff
ibrahim-azil <ibrahimazil.gtr@gmail.com>
2025-07-17 15:16:23 +0100

    17-07-2025_3

--- resources/views/pages/residences/index.blade.php
+++ resources/views/pages/residences/index.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app :title="__('Residences')" >
 
-   <livewire:tables.residances-table /> <livewire:tables.residances-table />
+   <livewire:tables.residances-table />
    <livewire:onou.residance-details />
 
 </x-layouts.app>
