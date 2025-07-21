e332b1215370cd3327b28ccc90b63f12e80c89e5
kamelher <kameleddine.heraguemi@ensia.edu.dz>
2025-07-21 15:51:35 +0100

    fix: correct Livewire component path and update production hostname

--- resources/views/pages/dossier-hebergement/create.blade.php
+++ resources/views/pages/dossier-hebergement/create.blade.php
@@ -27,6 +27,6 @@
                         class="mt-8 shadow-lg"
                         id="my-custom-alert" />
     @endif
- <livewire:onou.forms.demandehebergement.search-by-year-matricule />
+ <livewire:onou.forms.demande-hebergement.search-by-year-matricule />
 </x-layouts.app>
 
