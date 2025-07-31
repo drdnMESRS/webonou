f061d838e4ad3db1e1e0d8ac709765442596ced3
ibrahim-azil <ibrahimazil.gtr@gmail.com>
2025-07-31 10:26:44 +0100

    alert_trad_31-07-2025

---
+++ resources/lang/ar/pipelines/onou/alerts.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+   'confirmite_age'=>'تأكيد العمر',
+   'age_conforme'=>'متوافق مع السن',
+   'age_not_conforme'=>'غير متوافق مع السن',
+   'ans'=>'سنة',
+   'adresse_domicile'=>'عنوان المنزل',
+   'confirmite'=>'تأكيد',
+   'adresse_domicile_conforme'=>'عنوان المنزل متوافق',
+   'adresse_domicile_not_conforme'=>'عنوان المنزل غير متوافق',
+   'etudiant_inscrie'=>'الطالب مسجل للعام الدراسي الحالي',
+   'situation_pidagogique_reguliere'=>'الوضعية البيداغوجية منتظمة',
+   'paiement_hebergement_reguliere'=>'الوضع المتعلق بدفع رسوم الإقامة منتظم',
+   'etudiant_pas_deuxieme_diplome'=>'الطالب ليس لديه شهادة ثانية',
+   'hebergement_reguliere'=>'حالة الإقامة منتظمة',
+];
